71aa0f2171aad310f55cf0af60e26a185b86191b
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-01 18:19:41 +0200

    aa

--- Assets/Scripts/KillOnTouch.cs
+++ Assets/Scripts/KillOnTouch.cs
@@ -3,26 +3,15 @@ using UnityEngine;
 using Photon.Pun;
 public class KillOnTouch : MonoBehaviour
 {
-    PhotonView photonView;
-
-    private void Awake()
-    {
-        photonView = PhotonView.Get(this);
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.WasPlayer())
         {
-            photonView.RPC("CallMyRoutine", RpcTarget.All, collision);
+            StartCoroutine("KillPlayer", collision);
         }
     }
 
     [PunRPC]
-    public void CallMyRoutine(Collision2D coll)
-    {
-        StartCoroutine("KillPlayer", coll);
-    }
-
     public IEnumerator KillPlayer(Collision2D coll)
     {
         coll.collider.GetComponent<Animator>().SetTrigger("Död");
