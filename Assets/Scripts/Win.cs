7bee6b96295aba9e1ae53747a711ce196597e270
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-01 18:53:24 +0200

    Update Win.cs

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -21,14 +21,13 @@ public class Win : MonoBehaviour
     [PunRPC]
     public void OnWin(GameObject player)
     {
-        //Time.timeScale = 0;
         GameObject winPanel = (GameObject)Instantiate(Resources.Load("WinPanel"), GameObject.Find("Canvas").transform);
         TextMeshProUGUI Wintext = winPanel.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-        if (player.name == "Gubbe2")
+        if (player.GetComponent<Gubbe2>())
         {
             Wintext.text = "Player 1 Wins!";
         }
-        else if (player.name == "Gubbe1")
+        else if (player.GetComponent<Gubbe1>())
         {
             Wintext.text = "Player 2 Wins!";
         }
@@ -37,9 +36,6 @@ public class Win : MonoBehaviour
     [PunRPC]
     public void RestartGame()
     {
-        print("Akt");
-        Time.timeScale = 1;
         PhotonNetwork.LoadLevel(1);
-        
     }
 }
