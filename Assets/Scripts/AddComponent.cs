440d66352ea7357673d6b7e02304f76e50640100
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-02 15:03:08 +0200

    värdelös script

---
+++ Assets/Scripts/AddComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddComponent : MonoBehaviour
+{
+    public static AddComponent instance;
+    void Start()
+    {
+        // To make sure we have AddComponent on the gameobject we use AddComponentToGameObject, this is very helpfull and the game would not run without this isolated script that adds itself if it doesn't exist except that it can't because it doens't exist
+        AddComponentToGameObject<AddComponent>(new AddComponent(), this.gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void AddComponentToGameObject<T>(T comp, GameObject gameObject)
+    {
+        Type type = comp.GetType();
+        if (!gameObject.GetComponent(type))
+        {
+            gameObject.AddComponent(type);
+        }
+    }
+}
--- Assets/Scripts/Collision2DExtensions.cs
+++ Assets/Scripts/Collision2DExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public static class Collision2DExtensions
 {
     public static bool WasPlayer(this Collision2D collision)
